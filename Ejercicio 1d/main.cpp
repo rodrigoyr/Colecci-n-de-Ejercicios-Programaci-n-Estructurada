7b9a99173abab9d1a4c47230808e076f14ae1e39
rodrigoyr <ryeperub@myuax.com>
2023-12-12 11:51:26 +0100

    last code commit

--- Ejercicio 1d/main.cpp
+++ Ejercicio 1d/main.cpp
@@ -1,2 +1,18 @@
-struct Estudiante *punteroEst = &estudiante1;
-printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", punteroEst->nombre, punteroEst->edad, punteroEst->promedio);
+#include <stdio.h>
+#include <string.h>
+
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
+extern struct Estudiante estudiante1;
+
+int main() {
+    struct Estudiante *punteroEst = &estudiante1;
+    printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", punteroEst->nombre, punteroEst->edad, punteroEst->promedio);
+    return 0;
+}
+
+struct Estudiante estudiante1 = {"Juan", 20, 9.5};
