a2d4aaf4f768e30e80c535f4bcd631b56f72bd7e
rodrigoyr <ryeperub@myuax.com>
2023-12-12 12:01:11 +0100

    copilot code commit

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -1,5 +1,21 @@
 typedef struct Estudiante Estudiante;
 
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
+Estudiante estudiante1;
+strcpy(estudiante1.nombre, "Juan");
+estudiante1.edad = 20;
+estudiante1.promedio = 8.5;
+
+Estudiante estudiante2;
+strcpy(estudiante2.nombre, "Maria");
+estudiante2.edad = 22;
+estudiante2.promedio = 9.1;
+
 Estudiante estudiante3;
 strcpy(estudiante3.nombre, "Pedro");
 estudiante3.edad = 25;
