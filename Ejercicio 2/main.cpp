9b14991a570f1939702c6f715efd2c10fc5e2cba
rodrigoyr <ryeperub@myuax.com>
2023-12-12 11:52:31 +0100

    first code commit

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+union Dato {
+    int i;
+    float f;
+    char str[20];
+};
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+union Dato dato;
+
+dato.i = 10;
+printf("dato.i: %d\n", dato.i);
+
+dato.f = 220.5;
+printf("dato.f: %.2f\n", dato.f);
+
+strcpy(dato.str, "C programming");
+printf("dato.str: %s\n", dato.str);
