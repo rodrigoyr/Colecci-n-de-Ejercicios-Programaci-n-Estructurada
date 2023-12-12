4cde9b002267d0a07f65b754ba8d9b217376b5a3
rodrigoyr <ryeperub@myuax.com>
2023-12-12 11:47:15 +0100

    first code commit

--- Ejercicio 1c/main.cpp
+++ Ejercicio 1c/main.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+struct Estudiante *estudiante2 = (struct Estudiante *) malloc(sizeof(struct Estudiante));
+strcpy(estudiante2->nombre, "Ana");
+estudiante2->edad = 22;
+estudiante2->promedio = 9.8;
