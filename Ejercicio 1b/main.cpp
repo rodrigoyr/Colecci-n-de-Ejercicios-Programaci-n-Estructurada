038403ba88cf8b88e01060ae0fb964f29d31e7fe
rodrigoyr <ryeperub@myuax.com>
2023-12-12 11:45:45 +0100

    fixed code commit

--- Ejercicio 1b/main.cpp
+++ Ejercicio 1b/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    struct Estudiante estudiante1;
+
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
+    printf("Nombre: %s\n", estudiante1.nombre);
+    printf("Edad: %d\n", estudiante1.edad);
+    printf("Promedio: %.2f\n", estudiante1.promedio);
+
     return 0;
 }
