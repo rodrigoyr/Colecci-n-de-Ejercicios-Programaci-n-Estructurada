a13cc0229e540d5c68677eed2b7b277d37a171e3
rodrigoyr <ryeperub@myuax.com>
2023-12-12 11:42:49 +0100

    code commit

--- Ejercicio 1a/main.cpp
+++ Ejercicio 1a/main.cpp
@@ -1,5 +1,16 @@
+
 struct Estudiante {
-    char nombre[50];
+    char nombre[MAX_NOMBRE];
     int edad;
     float promedio;
 };
+
+int main() {
+    struct Estudiante estudiante1 = {"Juan", 20, 85.5};
+
+    printf("Nombre: %s\n", estudiante1.nombre);
+    printf("Edad: %d\n", estudiante1.edad);
+    printf("Promedio: %.2f\n", estudiante1.promedio);
+
+    return 0;
+}
