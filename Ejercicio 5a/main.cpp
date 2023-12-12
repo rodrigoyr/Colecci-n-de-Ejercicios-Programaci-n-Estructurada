ccb8f8fb6517e623d551655d4ce92386bc62db50
rodrigoyr <ryeperub@myuax.com>
2023-12-12 12:05:44 +0100

    fixed code commit

--- Ejercicio 5a/main.cpp
+++ Ejercicio 5a/main.cpp
@@ -1,4 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char nombre[100];
+    int edad;
+    float promedio;
+} Estudiante;
+
 void imprimirEstudiante(Estudiante est) {
     printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", est.nombre, est.edad, est.promedio);
 }
-imprimirEstudiante(estudiante1);
+
+int main() {
+    Estudiante estudiante1;
+
+    imprimirEstudiante(estudiante1);
+
+    return 0;
+}
