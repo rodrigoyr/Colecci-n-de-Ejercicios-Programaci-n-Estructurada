4cf64483fb5ea4df8b20dc3b346033512ef71990
rodrigoyr <ryeperub@myuax.com>
2023-12-12 12:12:02 +0100

    fixed code commit

--- Ejercicio 5c/main.c
+++ Ejercicio 5c/main.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char nombre[100];
+    int edad;
+    float promedio;
+} Estudiante;
+
 void imprimirEstudiante2(Estudiante *est) {
     printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", est->nombre, est->edad, est->promedio);
 }
-imprimirEstudiante2(&estudiante1);
+
+int main() {
+    Estudiante estudiante1;
+
+    imprimirEstudiante2(&estudiante1);
+
+    return 0;
+}
