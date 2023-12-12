a4a54957159434ada5878d141a51ed1a11d1feac
rodrigoyr <ryeperub@myuax.com>
2023-12-12 11:53:13 +0100

    fixed code commit

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,16 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
 union Dato {
     int i;
     float f;
     char str[20];
 };
 
-union Dato dato;
+int main() {
+    union Dato dato;
 
-dato.i = 10;
-printf("dato.i: %d\n", dato.i);
+    dato.i = 10;
+    printf("dato.i: %d\n", dato.i);
 
-dato.f = 220.5;
-printf("dato.f: %.2f\n", dato.f);
+    dato.f = 220.5;
+    printf("dato.f: %.2f\n", dato.f);
 
-strcpy(dato.str, "C programming");
-printf("dato.str: %s\n", dato.str);
+    dato.i = 10;
+
+    strcpy(dato.str, "C programming");
+    printf("dato.str: %s\n", dato.str);
+
+    return 0;
+}
